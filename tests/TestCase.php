bbdf3c2ed7e87cee3cdac37e396e910ad75f5a06
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-08-17 14:16:56 -0300

    Formatting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,8 +9,6 @@ abstract class TestCase extends AbstractPackageTestCase
 {
     /**
      * Get the service provider class.
-     *
-     * @return string
      */
     protected static function getServiceProviderClass(): string
     {
@@ -18,9 +16,6 @@ abstract class TestCase extends AbstractPackageTestCase
     }
 
     /**
-     * @param  array  $data
-     * @param  array  $rules
-     * @param  array  $messages
      * @return \Illuminate\Validation\Validator
      */
     protected function validate(array $data, array $rules, array $messages = [])
